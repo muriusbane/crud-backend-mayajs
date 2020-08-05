83cb1e01c8039ea746a4c1b679dd156bb95f39b1
Renellie Alfred Maglasang <technurvuz@gmail.com>
2020-08-05 13:03:53 +0800

    CHORE: Remove connection string

--- src/databases/mongo.ts
+++ src/databases/mongo.ts
@@ -4,7 +4,7 @@ import todos from "../controllers/todo/todos.model";
 export = Mongo({
   name: "test",
   connectionString:
-    "mongodb+srv://root:root@mayajs.6izmq.mongodb.net/test?authSource=admin&replicaSet=atlas-dbtreq-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true",
+    "db con string here",
   schemas: [
     todos, // Add Mongoose Schema here
   ],
